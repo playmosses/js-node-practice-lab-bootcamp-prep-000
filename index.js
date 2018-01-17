5ad1326a24f9c6595a523ad060a586b24317f082
Abimbola Moses Agbaje <playmoses@gmail.com>
2018-01-17 05:59:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Moses"
-var height = 74
+var height = 5'9
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+)
